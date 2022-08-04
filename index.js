66f0e296cc6cbc2b09b30d42e9ba925a4ad419a4
Kenneth <kennethgonzalez067@gmail.com>
2022-08-04 00:19:10 -0400

    bro work

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-// $("button").click(function(){
-//     $(".foodContainer").html("");
-//     let input = $("input").val();
-//     // let apiLink = `https://api.spoonacular.com/food/menuItems/search?query=${input}&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
-//     let apiLink = `https://api.spoonacular.com/recipes/complexSearch?query=${input}&addRecipeInformation=true&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
-//     fetch(apiLink)
-//     .then(function(apidata) {
-//         return apidata.json();
-//     })
-//     .then(function(apidata){
-//         console.log(apidata);
-//         let menuItem = apidata.results;
+$("button").click(function(){
+    $(".foodContainer").html("");
+    let input = $("input").val();
+    // let apiLink = `https://api.spoonacular.com/food/menuItems/search?query=${input}&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
+    let apiLink = `https://api.spoonacular.com/recipes/complexSearch?query=${input}&addRecipeInformation=true&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
+    fetch(apiLink)
+    .then(function(apidata) {
+        return apidata.json();
+    })
+    .then(function(apidata){
+        console.log(apidata);
+        let menuItem = apidata.results;
         
-//         menuItem.forEach(function(food){
-//             // $('.foodContainer').append(`<p>${food.readyInMinutes}</p>`)
-//             $(`.foodContainer`).append(`<div class="foodItem"><a href="${food.spoonacularSourceUrl}"><img src="${food.image}"></a></div>`);
+        menuItem.forEach(function(food){
+            // $('.foodContainer').append(`<p>${food.readyInMinutes}</p>`)
+            $(`.foodContainer`).append(`<div class="foodItem"><a href="${food.spoonacularSourceUrl}"><img src="${food.image}"></a></div>`);
             
-//             // $("#foodLessThan30").click(function(){
-//             //     if(food.readyInMinutes < 30){
-//             //         $(`.foodContainer`).push(food);
-//             //  }  
-//             // });
+            // $("#foodLessThan30").click(function(){
+            //     if(food.readyInMinutes < 30){
+            //         $(`.foodContainer`).push(food);
+            //  }  
+            // });
 
-//             // $(`#foodMoreThan30`).click(function(){
-//             //     if(food.readyInMinutes > 30){
-//             //         $('.foodContainer').push(food);
-//             //     }
-//             // });
+            // $(`#foodMoreThan30`).click(function(){
+            //     if(food.readyInMinutes > 30){
+            //         $('.foodContainer').push(food);
+            //     }
+            // });
             
-//         });
+        });
 
-//         if(menuItem.length == 0){
-//             $('.foodContainer').append(`<div class="errorMessageText"><h1>Sorry we couldn't find any results!</h1></div>`)
-//             $('.foodContainer').append(`<div class="errorMessageImage"><img src=images/errorImage.png width="200px length="200px"></img> </div>`)
-//         }
+        if(menuItem.length == 0){
+            $('.foodContainer').append(`<div class="errorMessageText"><h1>Sorry we couldn't find any results!</h1></div>`)
+            $('.foodContainer').append(`<div class="errorMessageImage"><img src=images/errorImage.png width="200px length="200px"></img> </div>`)
+        }
 
         
-//     });
-// });
+    });
+});
