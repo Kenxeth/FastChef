7fa1bc6d34ed36f70768096bcc3743f32e445225
Kenneth <kennethgonzalez067@gmail.com>
2022-08-12 00:01:11 -0400

    replaced JSON for JQUERY, and way more

--- index.js
+++ index.js
@@ -1,39 +1,86 @@
-$("button").click(function(){
-    $(".foodContainer").html("");
-    let input = $("input").val();
-    // let apiLink = `https://api.spoonacular.com/food/menuItems/search?query=${input}&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
-    let apiLink = `https://api.spoonacular.com/recipes/complexSearch?query=${input}&addRecipeInformation=true&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
-    fetch(apiLink)
+// replace this jquery w/ DOM
+const button = document.getElementById("search-button");
+const input = document.getElementById("input");
+let container = document.querySelector(".container");
+let array = [];
+
+// if user presses enter instead
+input.addEventListener("keypress", function(event){
+
+    if(event.key === "Enter"){
+        event.preventDefault();
+        document.getElementById("search-button").click();
+    }
+});
+
+
+// 
+
+
+function appendOnPage(value, column){
+// VALUE = basically what row (menuItem)
+// COLUMN = WHICH COLUMN IT GOES IN
+    //    food images
+// column.innerHTML = "";
+let imageSource = value.image;
+let image = document.createElement("img");
+image.src = imageSource;
+column.append(image);
+// food name
+let foodNameContainer = document.createElement("h1");
+let foodName = value.title;
+foodNameContainer.append(foodName)
+column.append(foodNameContainer);
+// food description
+let foodDescriptionContainer = document.createElement("p")
+let foodDescription = value.readyInMinutes;
+foodDescriptionContainer.append("Ready in " + foodDescription + " minutes")
+column.append(foodDescriptionContainer)
+// food link
+let foodLinkContainer = document.createElement("a")
+foodLinkContainer.classList.add("btn", "btn-primary");
+foodLinkContainer.append("More Information")
+let foodLink = value.spoonacularSourceUrl
+foodLinkContainer.href = foodLink
+column.append(foodLinkContainer)
+
+}
+
+button.onclick = function(){
+
+let input = document.querySelector("input").value
+let apiLink = `https://api.spoonacular.com/recipes/complexSearch?query=${input}&addRecipeInformation=true&apiKey=55e9ca3e426d448aabf00e02fab9e045`;
+fetch(apiLink)
     .then(function(apidata) {
         return apidata.json();
     })
     .then(function(apidata){
-        console.log(apidata);
         let menuItem = apidata.results;
+        // value.style.width = "18rem";
+        container.innerHTML = ""
         
-        menuItem.forEach(function(food){
-            // $('.foodContainer').append(`<p>${food.readyInMinutes}</p>`)
-            $(`.foodContainer`).append(`<div class="foodItem"><a href="${food.spoonacularSourceUrl}"><img src="${food.image}"></a></div>`);
+        for(let i=0;i<menuItem.length;i+= 2){
+
+
+            let newRow= document.createElement("div")
+            newRow.classList=["row"]
             
-            // $("#foodLessThan30").click(function(){
-            //     if(food.readyInMinutes < 30){
-            //         $(`.foodContainer`).push(food);
-            //  }  
-            // });
-
-            // $(`#foodMoreThan30`).click(function(){
-            //     if(food.readyInMinutes > 30){
-            //         $('.foodContainer').push(food);
-            //     }
-            // });
+            let firstCol= document.createElement("div")
+            let secondCol= document.createElement("div")
             
-        });
+            firstCol.append()
 
-        if(menuItem.length == 0){
-            $('.foodContainer').append(`<div class="errorMessageText"><h1>Sorry we couldn't find any results!</h1></div>`)
-            $('.foodContainer').append(`<div class="errorMessageImage"><img src=images/errorImage.png width="200px length="200px"></img> </div>`)
-        }
+            firstCol.classList=["card col-12 col-sm-12 col-md-6 col-lg-6 "]
+            secondCol.classList=["card col-12 col-sm-12 col-md-6 col-lg-6"]
 
-        
-    });
-});
+
+            appendOnPage(menuItem[i], firstCol)
+            appendOnPage(menuItem[i + 1], secondCol)
+            
+            newRow.append(firstCol, secondCol)
+
+            container.append(newRow)
+          
+           }
+    })
+}
